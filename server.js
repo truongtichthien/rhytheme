5eba50a62a12008fdf4c3c80066e217e7604fc55
ThienTruong <ttthien@tma.com.vn>
2017-02-08 17:01:51 +0700

    Init package.json & Update server.js

--- server.js
+++ server.js
@@ -13,18 +13,3 @@ app.listen(3000, function () {
   //console.log('Example app listening on port 3000!')
   console.log('Server running at http://127.0.0.1:3000/');
 });
-
-//var http = require('http');
-//http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   //response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   //response.end('Hello the fucking World!\n');
-//}).listen(3000);
-
-// Console will print the message
-//console.log('Server running at http://127.0.0.1:3000/');
-
