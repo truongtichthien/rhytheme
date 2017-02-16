bf7071fb0783fbf0875a4e1e6328144f87007111
ThienTruong <ttthien@tma.com.vn>
2017-02-16 09:56:07 +0700

    Delete redundant folder & Create database directory

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 
 // configuration =================
 
-//mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+mongoose.connect('mongodb://myUserAdmin:abc123@127.0.0.1:27017/admin');     // connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 //app.use(morgan('dev'));                                         // log every request to the console
