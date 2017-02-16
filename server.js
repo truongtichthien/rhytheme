cdede2247fc8a9da744859b72f1ab19d2fa28c1e
ThienTruong <ttthien@tma.com.vn>
2017-02-16 14:14:32 +0700

    Create app directory with bower_components installed inside

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // set up ========================
-var express  = require('express');
-var app      = express();                               // create our app w/ express
+var express = require('express');
+var server = express();                               // create our server w/ express
 var mongoose = require('mongoose');                     // mongoose for mongodb
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
@@ -8,23 +8,26 @@ var methodOverride = require('method-override'); // simulate DELETE and PUT (exp
 
 // configuration =================
 
-mongoose.connect('mongodb://myUserAdmin:abc123@127.0.0.1:27017/admin');     // connect to mongoDB database on modulus.io
+//mongoose.connect('mongodb://myUserAdmin:abc123@127.0.0.1:27017/admin');     // connect to mongoDB database on modulus.io
 
-app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-//app.use(morgan('dev'));                                         // log every request to the console
-//app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-//app.use(bodyParser.json());                                     // parse application/json
-//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-//app.use(methodOverride());
+server.use(express.static(__dirname + '/public'), function(req, res, next) {
+	console.log(req);
+	next();
+});                 // set the static files location /public/img will be /img for users
+//server.use(morgan('dev'));                                         // log every request to the console
+//server.use(bodyParser.urlencoded({'extended':'true'}));            // parse serverlication/x-www-form-urlencoded
+//server.use(bodyParser.json());                                     // parse serverlication/json
+//server.use(bodyParser.json({ type: 'serverlication/vnd.api+json' })); // parse serverlication/vnd.api+json as json
+//server.use(methodOverride());
 
-app.get('/', function (req, res) {
+server.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
-app.get('/test', function (req, res) {
+server.get('/test', function (req, res) {
   res.send('Hello the fucking World!')
 });
 
-// listen (start app with node server.js) ======================================
-app.listen(3000);
-console.log("App listening on port 3000");
+// listen (start server with node server.js) ======================================
+server.listen(3000);
+console.log("server listening on port 3000");
