613dab2d99d0dfaaae2b4998fc4f8b461b4206d8
Steve <steve.truong@iskra.world>
2023-04-11 11:10:21 +0700

    Add vercel config

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 (function (_req) {
-
   /** SET UP ======================== */
 
-    // create onw server express
+  // create onw server express
   var express = _req('express'),
-    PORT = process.env.PORT || 3000,
+    PORT = process.env.PORT || 3300,
     server = express();
 
   var path = _req('path');
@@ -15,10 +14,10 @@
     morgan = _req('morgan');
 
   var Todo = mongoose.model('testCollection', {
-    text: String
+    text: String,
   });
 
-// pull information from HTML POST (express4)
+  // pull information from HTML POST (express4)
   var bodyParser = _req('body-parser'),
     // simulate DELETE and PUT (express4)
     methodOverride = _req('method-override');
@@ -77,11 +76,10 @@
   server.get('*', function (req, res) {
     console.log('not found');
     res.redirect('/rhytheme');
-//    res.redirect('/demo');
+    // res.redirect('/demo');
   });
 
   server.get('/api/get', function (req, res) {
-
     Todo.find(function (err, data) {
       console.log('find');
 
@@ -97,7 +95,6 @@
     });
 
     // res.send(JSON.parse(JSON.stringify({test: 123})));
-
   });
 
   server.get('/api/getString', function (req, res) {
@@ -112,24 +109,27 @@
     // }
 
     // create a todo, information comes from AJAX request from Angular
-    Todo.create({
-      text: req.body.string,
-      done: false
-    }, function (err, todos) {
-      console.log('create');
+    Todo.create(
+      {
+        text: req.body.string,
+        done: false,
+      },
+      function (err, todos) {
+        console.log('create');
 
-      if (err) {
-        res.send(err);
-      }
-
-      // get and return all the todos after you create another
-      Todo.find(function (err, todos) {
         if (err) {
-          res.send(err)
+          res.send(err);
         }
-        console.log(todos);
-      });
-    });
+
+        // get and return all the todos after you create another
+        Todo.find(function (err, todos) {
+          if (err) {
+            res.send(err);
+          }
+          console.log(todos);
+        });
+      },
+    );
 
     // var result = JSON.stringify(req);
     res.send(req.body);
@@ -170,7 +170,7 @@
   function _createResponse(success, msg) {
     return {
       success: success,
-      message: msg
+      message: msg,
     };
   }
 })(require);
