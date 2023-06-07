80f581dfe69c668e39f82d329ba605c694b2d1ed
Thien Truong <truongtichthien0401@gmail.com>
2023-06-07 21:50:53 +0700

    Update server

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
 /** SET UP ======================== */
 
-// create onw server express
 var express = require('express');
 var PORT = process.env.PORT || 3300;
 var server = express();
-
 var path = require('path');
-
-// mongoose for mongodb
-var mongoose = require('mongoose'),
-  // log requests to the console (express4)
-  morgan = require('morgan');
-
-var Todo = mongoose.model('testCollection', {
-  text: String,
-});
 
 // pull information from HTML POST (express4)
 var bodyParser = require('body-parser');
 
 /** CONFIGURATION ================= */
+
 // set the static files location /public/img will be /img for users
 server.use(express.static(path.join(__dirname, '/src'), { index: false }));
 
@@ -27,7 +17,6 @@ server.use(express.static(path.join(__dirname, '/src'), { index: false }));
 server.use(morgan('dev'));
 
 // parse serverlication/json
-/** parse request JSON */
 server.use(bodyParser.json());
 
 server.get('/', function (req, res) {
@@ -36,58 +25,7 @@ server.get('/', function (req, res) {
 });
 
 server.get('*', function (req, res) {
-  console.log('not found');
-  res.redirect('/rhytheme');
-});
-
-server.get('/api/get', function (req, res) {
-  Todo.find(function (err, data) {
-    console.log('find');
-
-    // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-
-    if (err) {
-      data.send(JSON.parse(JSON.stringify(err)));
-    }
-
-    console.log(data);
-    res.send(JSON.parse(JSON.stringify(data)));
-    // res.json(res); // return all todos in JSON format
-  });
-
-  // res.send(JSON.parse(JSON.stringify({test: 123})));
-});
-
-server.get('/api/getString', function (req, res) {
-  res.send('abc');
-});
-
-server.post('/api/post', function (req, res) {
-  // create a todo, information comes from AJAX request from Angular
-  Todo.create(
-    {
-      text: req.body.string,
-      done: false,
-    },
-    function (err, todos) {
-      console.log('create');
-
-      if (err) {
-        res.send(err);
-      }
-
-      // get and return all the todos after you create another
-      Todo.find(function (err, todos) {
-        if (err) {
-          res.send(err);
-        }
-        console.log(todos);
-      });
-    },
-  );
-
-  // var result = JSON.stringify(req);
-  res.send(req.body);
+  res.redirect('/');
 });
 
 /** SERVER EXECUTION ============== */
